b3c50f3ac102fd9772c1f7ff820a49c1a4e2960c
Code It Like Miley <codeitlikemiley@gmail.com>
2019-09-09 16:45:31 +0800

    Update to Latest Vuetify 2.0

--- src/stubs/app.js
+++ src/stubs/app.js
@@ -1,12 +1,16 @@
 import { InertiaApp } from '@inertiajs/inertia-vue'
 import Vue from 'vue'
+import vuetify from '@/plugins/vuetify'
 
 Vue.use(InertiaApp)
+
+Vue.config.productionTip = false
 
 const app = document.getElementById('app')
 
 new Vue({
-    render: h => h(Inertia, {
+    vuetify,
+    render: h => h(InertiaApp, {
         props: {
             initialPage: JSON.parse(app.dataset.page),
             resolveComponent: name => import(`@/Pages/${name}`).then(module => module.default),
