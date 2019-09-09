0805377c694c905e8673558054d37d0c2a6ea869
Code It Like Miley <codeitlikemiley@gmail.com>
2019-09-09 16:44:51 +0800

    Update to Latest Vuetify 2.0

--- src/stubs/vuetify/Pages/Welcome.vue
+++ src/stubs/vuetify/Pages/Welcome.vue
@@ -1,30 +1,51 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer />
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
-    </v-toolbar>
+<v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item @click="">
+          <v-list-item-action>
+            <v-icon>{{ svgPath  }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="">
+          <v-list-item-action>
+            <v-icon>account_balance_wallet</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
 
     <v-content>
-      <HelloWorld />
+      <hello-world></hello-world>
     </v-content>
+
+    <v-footer app>
+      <span>&copy; 2019</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from '@/Shared/HelloWorld'
-
+import {mdiLogin} from  '@mdi/js/'
 export default {
   name: 'App',
   components: {
@@ -32,8 +53,13 @@ export default {
   },
   data () {
     return {
-      //
+      drawer: false,
+      svgPath: mdiLogin
     }
   },
+  created () {
+      // override the theme
+      this.$vuetify.theme.dark = false
+    },
 }
 </script>
